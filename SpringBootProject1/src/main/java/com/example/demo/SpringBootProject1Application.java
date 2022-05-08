6dd1987a953102efc9909de36fc74469716d2eab
GOMATHI KAVI <GOMATHI KAVI@DESKTOP-AB6DJ25>
2022-05-07 19:24:27 -0700

    GitHub

---
+++ SpringBootProject1/src/main/java/com/example/demo/SpringBootProject1Application.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringBootProject1Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context =SpringApplication.run(SpringBootProject1Application.class, args);
+		   Student sob=context.getBean(Student.class);
+		   //System.out.println(sob);
+		      sob.display();
+
+			}
+
+		
+
+		
+	}
+
+
